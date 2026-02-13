bbfd7831cdcb1b778f545017868896a50fa531d9
Triet Huynh <triet403@gmail.com>
2026-02-12 19:51:11 -0500

    update

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@
 #
 
 # Import the Portal object.
-import geni.portal as portal
+import geni.portal as portal 
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
 # Import the Emulab specific extensions.
@@ -23,23 +23,69 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Node client
+client_script = """
+sudo apt update
+sudo apt install -y dnsperf
+"""
+
 node_client = request.RawPC('client')
 node_client.hardware_type = 'c220g2'
 node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD'
 iface0 = node_client.addInterface('client-interface', pg.IPv4Address('192.168.10.1','255.255.255.0'))
 
+node_client.addService(pg.Execute(shell="bash", command=client_script))
+
 # Node resolver
+resolver_script = """
+# dependencies for liboqs
+sudo apt update 
+
+sudo apt install -y astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+git clone -b main https://github.com/open-quantum-safe/liboqs.git
+cd liboqs
+
+mkdir build && cd build
+cmake -GNinja -DOQS_MINIMAL_BUILD="SIG_ml_dsa_44;SIG_ml_dsa_65;SIG_ml_dsa_87" ..
+ninja
+
+ninja install
+
+cd
+
+git clone https://github.com/HuMiTriet/unbound.git
+
+cd unbound
+./configure --enable-debug --enable-filter --enable-oqs
+
+make 
+
+make install
+
+cd
+"""
+
+
 node_resolver = request.RawPC('resolver')
 node_resolver.hardware_type = 'c220g2'
 node_resolver.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD'
 iface1 = node_resolver.addInterface('res-interface', pg.IPv4Address('192.168.10.2','255.255.255.0'))
 iface2 = node_resolver.addInterface('resolver-interface', pg.IPv4Address('192.168.20.1','255.255.255.0'))
 
+node_resolver.addService(pg.Execute(shell="bash", command=resolver_script))
+
 # Node NS
+ns_script = """
+sudo apt update
+sudo apt install -y nsd
+"""
+
+
 node_NS = request.RawPC('NS')
 node_NS.hardware_type = 'c220g2'
 node_NS.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD'
 iface3 = node_NS.addInterface('ns-interface', pg.IPv4Address('192.168.20.2','255.255.255.0'))
+
+node_NS.addService(pg.Execute(shell="bash", command=ns_script))
 
 # Link client-resolver-link
 link_client_resolver_link = request.LAN('client-resolver-link')
