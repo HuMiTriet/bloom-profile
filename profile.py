07bab2886601423a86ca8b9b728bbaaf5c481fb3
Triet Huynh <triet403@gmail.com>
2026-04-28 01:43:09 -0400

    update resolver

--- profile.py
+++ profile.py
@@ -53,7 +53,7 @@ evil_iface0 = evil_node_client.addInterface(
 node_resolver = request.RawPC("resolver")
 node_resolver.hardware_type = HARDWARE_TYPE
 node_resolver.disk_image = (
-    "urn:publicid:IDN+wisc.cloudlab.us+image+mt1-PG0:3DNS_git.resolver:7"
+    "urn:publicid:IDN+wisc.cloudlab.us+image+mt1-PG0:3DNS_git.resolver:9"
 )
 iface1 = node_resolver.addInterface(
     "res-interface", pg.IPv4Address("192.168.10.2", "255.255.255.0")
